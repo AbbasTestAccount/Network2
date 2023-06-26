c2616f49b4acfba881f7c0d71e26e49d7b9e3d27
AbbasTestAccount <abbasfake5@gmail.com>
2023-06-26 17:33:22 +0330

    add a method to save cache in cache.json

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ with open("settings.json", "r") as settings_file:
 CACHE_EXPIRATION_TIME = settings["cache-expiration-time"]
 EXTERNAL_DNS_SERVERS = settings["external-dns-servers"]
 
+cache = {}
+
+
+def save_cache_to_file():
+    with open("cache.json", "w") as cache_file:
+        # covert into json
+        json.dump(cache, cache_file)
+
 
 print(CACHE_EXPIRATION_TIME, EXTERNAL_DNS_SERVERS)
