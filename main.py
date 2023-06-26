ac133248f7591e92e73e324c04c69e0c53accc1d
AbbasTestAccount <abbasfake5@gmail.com>
2023-06-26 20:29:36 +0330

    add a piece of code to convert host to ip

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import json
+import socket
+
+HOST = "127.0.0.1"
+PORT = 5001
 
 with open("settings.json", "r") as settings_file:
     settings = json.load(settings_file)
@@ -22,4 +26,29 @@ def load_cache_from_file():
         cache = json.load(cache_file)
 
 
-print(CACHE_EXPIRATION_TIME, EXTERNAL_DNS_SERVERS)
+# at the beginning of the project, we should read our last datas from cache.json
+load_cache_from_file()
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:  # use a UDP connection
+    s.settimeout(4.0)  # set a timeout to resend data if it didn't send
+    s.bind(HOST, PORT)
+# todo
+# should use some thread here to send N requests
+
+
+while True:
+    requested_domain = ""
+    if cache.get(requested_domain):
+        print(f'{cache[requested_domain]}cache hit !!!\n')
+        continue
+
+    try:
+        ip = socket.gethostname(requested_domain)  # get ip
+        cache[requested_domain] = ip
+
+        save_cache_to_file()
+
+        print(f'{ip}\n')
+
+    except Exception as e:
+        print("error is happened !!!")
