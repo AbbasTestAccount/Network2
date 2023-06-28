f0fb3c6ab6112ff28aac18456c7f8aad61b6e1e4
AbbasTestAccount <abbasfake5@gmail.com>
2023-06-29 01:31:55 +0330

    add another file to start coding in a new way

---
+++ main2.py
@@ -0,0 +1,9 @@
+import json
+
+with open("settings.json", "r") as settings_file:
+    settings = json.load(settings_file)
+
+CACHE_EXPIRATION_TIME = settings["cache-expiration-time"]
+EXTERNAL_DNS_SERVERS = settings["external-dns-servers"]
+
+cache = {}
