ecd910c598361b13ffa2df4b0838a854982edac1
AbbasTestAccount <abbasfake5@gmail.com>
2023-06-29 01:34:51 +0330

    add a new socket to listen to the client's req

--- main2.py
+++ main2.py
@@ -1,4 +1,5 @@
 import json
+import socket
 
 with open("settings.json", "r") as settings_file:
     settings = json.load(settings_file)
@@ -7,3 +8,10 @@ CACHE_EXPIRATION_TIME = settings["cache-expiration-time"]
 EXTERNAL_DNS_SERVERS = settings["external-dns-servers"]
 
 cache = {}
+
+# ساخت یک سوکت UDP برای گوش دادن به درخواست های DNS
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('127.0.0.1', 53))  # Listen on localhost port 53
+
+while True:
+    data, addr = sock.recvfrom(1024)
