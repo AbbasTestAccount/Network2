cdfec9dffc0b417508b96feabfc96fe162cab80a
AbbasTestAccount <abbasfake5@gmail.com>
2023-06-29 01:46:50 +0330

    complete DNSResponse class

--- main2.py
+++ main2.py
@@ -37,7 +37,48 @@ class DNSRequest:
 
 
 class DNSResponse:
-    pass
+    def __init__(self, data):
+        self.id = data[:2]
+        print(self.id)
+        self.flags = data[2:4]
+        self.questions = data[4:6]
+        self.number_of_answers = data[6:8]
+        self.authority = data[8:10]
+        self.additional = data[10:12]
+        self.qname = parse_qname(data[12:])
+        self.qtype = data[-4:-2]
+        self.qclass = data[-2:]
+
+        self.rname = ''
+        self.rtype = ''
+        self.rclass = ''
+        self.ttl = ''
+        self.rdlength = ''
+        self.rdata = ''
+
+        self.answers = []
+        offset = 12 + len(self.qname) + 4
+        for i in range(int.from_bytes(self.number_of_answers, byteorder='big')):
+            rr_data = data[offset:]
+            rr = self.parse_rr(rr_data)
+            self.answers.append(rr)
+            offset += len(rr_data)
+
+    def parse_rr(self, data):
+        return self.parsing(data)
+
+    def parsing(self, data):
+        i = 0
+        self.rname = parse_qname(data[i:])
+        i += len(self.rname) + 2
+        self.rtype = data[i:i + 2]
+        self.rclass = data[i + 2:i + 4]
+        self.ttl = data[i + 4:i + 8]
+        self.rdlength = data[i + 8:i + 10]
+        self.rdata = data[i + 10:i + 10 + self.rdlength]
+
+    def __str__(self):
+        return f"DNS Response: ID={self.id}, QNAME={self.qname}, QTYPE={self.qtype}, QCLASS={self.qclass}, RNAME={self.rname}, RTYPE={self.rtype}, RCLASS={self.rclass}, TTL={self.ttl}, RDLENGTH={self.rdlength}, RDATA={self.rdata}"
 
 
 # ساخت یک سوکت UDP برای گوش دادن به درخواست های DNS
